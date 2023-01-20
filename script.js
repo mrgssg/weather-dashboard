abfda987cee162b14f09f9ba8bd87805dc846725
Mark Green <mrgssg@gmail.com>
2023-01-20 16:53:28 -0500

    added card styling and api key

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+// set the variables needed
+
 var cityEl = document.getElementById("enter-city");
 var searchEl = document.getElementById("search-button");
 var cityNameEl = document.getElementById("city-name");
 var historyEl = document.getAnimations("history");
 var fiveDayEl = document.getElementById("fiveday");
 var todaysWeatherEl = document.getElementById("todays-weather");
+var apiKey = "e8254a1270fb2c8b6d2371a09bd4627a";
 
-const APIKey = "e8254a1270fb2c8b6d2371a09bd4627a";
+// event listener for city search button and area to populate current weather
 
-function weather() {
-    
-}
+const url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+
+
+
+
+// section for 5-day forecast to populate
+
+
+
+// city search history 
+
+
