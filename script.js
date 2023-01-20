75101c8bb1fe4bb372b10746fa6d6115c9be3abd
Mark Green <mrgssg@gmail.com>
2023-01-20 14:32:49 -0500

    added html page structure

---
+++ script.js
@@ -0,0 +1,12 @@
+var cityEl = document.getElementById("enter-city");
+var searchEl = document.getElementById("search-button");
+var cityNameEl = document.getElementById("city-name");
+var historyEl = document.getAnimations("history");
+var fiveDayEl = document.getElementById("fiveday");
+var todaysWeatherEl = document.getElementById("todays-weather");
+
+const APIKey = "e8254a1270fb2c8b6d2371a09bd4627a";
+
+function weather() {
+    
+}
